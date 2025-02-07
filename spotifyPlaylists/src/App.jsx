9b3279128ef02fea21b503754a2628ba9482c807
Christine Hong <christine.hong@berkeley.edu>
2025-02-07 13:27:57 -0800

    added hover effects and looping animation

--- spotifyPlaylists/src/App.jsx
+++ spotifyPlaylists/src/App.jsx
@@ -14,13 +14,13 @@ function App() {
       <h1>ryoko's spotify playlists</h1>
       <div className="icon-container">
         <div className='playlist-icon'>
-          <a href='https://open.spotify.com/playlist/6gbAC8Dhg8HSopBVH4b4qi?si=15eaf0bcc3d6451c'><img src={catWink} /></a>
+          <a href='https://open.spotify.com/playlist/6gbAC8Dhg8HSopBVH4b4qi?si=15eaf0bcc3d6451c' target="_blank"><img src={catWink} /></a>
         </div>
         <div className='playlist-icon'>
-          <a href='https://open.spotify.com/playlist/2HE1YrQR9qMA86W3qtb1LR?si=d1eb2c9a3c674456'><img src={catFoot} /></a>
+          <a href='https://open.spotify.com/playlist/2HE1YrQR9qMA86W3qtb1LR?si=d1eb2c9a3c674456' target="_blank"><img src={catFoot} /></a>
         </div> 
         <div className='playlist-icon'>
-          <a href='https://open.spotify.com/playlist/210HNDZ7NwiB5I8nu3jiZ2?si=b97b4cbf31aa4454'><img src={catBlue} /></a>
+          <a href='https://open.spotify.com/playlist/210HNDZ7NwiB5I8nu3jiZ2?si=b97b4cbf31aa4454' target="_blank"><img src={catBlue} /></a>
         </div>
       </div>      
     </>
