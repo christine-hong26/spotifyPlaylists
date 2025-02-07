a37793c1119694987da6eacc587a0d5cc6c0d61b
Christine Hong <christine.hong@berkeley.edu>
2025-02-07 13:16:51 -0800

    finished bare bones html

--- spotifyPlaylists/src/App.jsx
+++ spotifyPlaylists/src/App.jsx
@@ -2,32 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import catWink from "./images/catWink.jpg"
+import catFoot from "./images/catFoot.jpg"
+import catBlue from "./images/catBlue.jpg"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>ryoko's spotify playlists</h1>
+      <div className="icon-container">
+        <div className='playlist-icon'>
+          <a href='https://open.spotify.com/playlist/6gbAC8Dhg8HSopBVH4b4qi?si=15eaf0bcc3d6451c'><img src={catWink} /></a>
+        </div>
+        <div className='playlist-icon'>
+          <a href='https://open.spotify.com/playlist/2HE1YrQR9qMA86W3qtb1LR?si=d1eb2c9a3c674456'><img src={catFoot} /></a>
+        </div> 
+        <div className='playlist-icon'>
+          <a href='https://open.spotify.com/playlist/210HNDZ7NwiB5I8nu3jiZ2?si=b97b4cbf31aa4454'><img src={catBlue} /></a>
+        </div>
+      </div>      
     </>
   )
 }
